fb624d2f0bcf6460b0fe8648d87306d7c8a28f3f
vairavan6 <vairavan.u@gmail.com>
2019-02-17 12:26:32 +0530

    refs #master

---
+++ src/main/java/devactivist/entity/PasRedmineDetails.java
@@ -0,0 +1,28 @@
+package devactivist.entity;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Document(collection="WorksheetPasDetails")
+public class PasRedmineDetails {
+	
+	@Id
+	private ObjectId id;
+
+	@Indexed(unique=true)
+	private String pasFileName;
+	
+	private String dprName;
+	
+	private String lableader;
+	private String assignee;
+	
+	private long redmineTicket;
+}
