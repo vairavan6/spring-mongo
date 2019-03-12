c722f5569971db6e714d1f21309e4aea627a8bd8
vairavan6 <vairavan.u@gmail.com>
2019-03-13 01:12:08 +0530

    refs #master readme

--- src/main/java/devactivist/service/SVNSyncService.java
+++ src/main/java/devactivist/service/SVNSyncService.java
@@ -1,33 +1,57 @@
 package devactivist.service;
 
 import org.springframework.stereotype.Service;
-import org.tmatesoft.svn.core.SVNDirEntry;
+import org.tmatesoft.svn.core.SVNDepth;
 import org.tmatesoft.svn.core.SVNException;
 import org.tmatesoft.svn.core.SVNURL;
 import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.io.SVNRepositoryFactory;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+import org.tmatesoft.svn.core.wc.SVNUpdateClient;
 import org.tmatesoft.svn.core.wc.SVNWCUtil;
-import org.tmatesoft.svn.core.wc2.SvnCheckout;
-import org.tmatesoft.svn.core.wc2.SvnOperationFactory;
-import org.tmatesoft.svn.core.wc2.SvnTarget;
-
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 //@Slf4j
 public class SVNSyncService {
 
 	public static void main(String[] args) {
-		
+		try {
+			callSVN();
+		} catch (SVNException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	public static void callSVN() throws SVNException {
-		SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+
+		/*final SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
+		try {
+			final SvnCheckout checkout = svnOperationFactory.createCheckout();
+			checkout.setSingleTarget(SvnTarget.fromFile(new File("H:\\Locals")));
+			checkout.setSource(SvnTarget.fromURL(SVNURL.parseURIEncoded("file:///H:/SVNGitTest")));
+			//... other options
+			checkout.run();
+		} finally {
+			svnOperationFactory.dispose();
+		}*/
+
+		/*String url = "http://svn.svnkit.com/repos/svnkit/trunk/doc";
+		String name = "anonymous";
+		String password = "anonymous";
+
+		SVNRepository repository = null;
+		repository = SVNRepositoryFactory.create( SVNURL.parseURIDecoded("file:///H:/SVNGitTest\"") );
+		ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager( name , password );
+		repository.setAuthenticationManager( authManager );*/
+
+
+		/*SvnOperationFactory svnOperationFactory = new SvnOperationFactory();
 		SvnCheckout svnCheckout = svnOperationFactory.createCheckout();
-		svnCheckout.run();
-		
-		
+		svnCheckout.run();*/
+
+
 		/*final ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(name, password);
 		final SVNURL svnUrl = SVNURL.create(url.getProtocol(), name, url.getHost(), 443, url.getPath(), true);
 
@@ -46,5 +70,22 @@ public class SVNSyncService {
 		checkout.setSource(SvnTarget.fromURL(svnUrl));
 		checkout.setSingleTarget(SvnTarget.fromFile(workingCopyDirectory));
 		remoteRevision = checkout.run();*/
+		
+		
+		SVNURL svnurl = SVNURL.parseURIDecoded("file:///H:/SVNGitTest");
+		SVNRepository repository = SVNRepositoryFactory.create(svnurl);
+		ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager("uname", "password");
+		repository.setAuthenticationManager(authManager);
+		SVNClientManager ourClientManager = SVNClientManager.newInstance();
+		ourClientManager.setAuthenticationManager(authManager);
+		SVNUpdateClient updateClient = ourClientManager.getUpdateClient();
+		updateClient.setIgnoreExternals(true);
+
+		long latestRevision = repository.getLatestRevision();
+		if (updateClient.doCheckout(svnurl, destinationDir,
+		        SVNRevision.HEAD, SVNRevision.HEAD, SVNDepth.INFINITY,
+		                allowUnversionedObstructions) == latestRevision) {
+		    ourClientManager.dispose();
+		}
 	}
 }
